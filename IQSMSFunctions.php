fca040cedf8034f80be5f612a3e0bd092291576b
ClientbasePro.ru <42553531+ClientbasePro@users.noreply.github.com>
2020-08-04 11:35:43 +0300

    Update IQSMSFunctions.php

--- IQSMSFunctions.php
+++ IQSMSFunctions.php
@@ -6,13 +6,14 @@
   
 require_once 'common.php'; 
 
-    // отправка СМС с текстом $text на телефон $phone от имени отправителя $sender, доп.признак $needSave сохраняет СМС в лог, доп.массив $data для сохранения в лог
-function IQSMS_SendSMS($phone, $text, $sender, $needSave=1, $data='') {
+  // отправка СМС с текстом $text на телефон $phone от имени отправителя $sender, доп.признак $needSave сохраняет СМС в лог, доп.массив $data для сохранения в лог
+function IQSMS_SendSMS($phone='', $text='', $sender='', $needSave=1, $data=[]) {
     // проверка и форматирование входных данных
+  if (!defined('IQSMS_URL') || !defined('IQSMS_API_LOGIN') || !defined('IQSMS_API_PASSWORD')) return false;
   $phone = SetNumber($phone);
   $text = form_input($text);
-  if (!$sender) $sender = IQSMS_DEFAULT_NAME;
-  if (!$phone || !$text) return false;
+  if (!$sender && defined('IQSMS_DEFAULT_NAME')) $sender = IQSMS_DEFAULT_NAME;
+  if (!$phone || !$text || !$sender) return false;
     // подготовка данных для запроса к серверу IQSMS
   $phone_ = rawurlencode($phone);
   $text_ = rawurlencode($text);
@@ -23,7 +24,7 @@ function IQSMS_SendSMS($phone, $text, $sender, $needSave=1, $data='') {
   $tmp = curl_exec($curl);    
   curl_close($curl);
     // сохранить в лог?
-  if ($needSave && SMSLOG_TABLE) {
+  if ($needSave && defined('SMSLOG_TABLE') && defined('SMSLOG_FIELD_UID') && defined('SMSLOG_FIELD_PHONE') && defined('SMSLOG_FIELD_TEXT')) {
 	$sms = explode(";", $tmp);
 	$ins['f'.SMSLOG_FIELD_UID] = $sms[1];
 	if ($ins['f'.SMSLOG_FIELD_UID] && 'accepted'==$sms[0]) {
@@ -36,9 +37,9 @@ function IQSMS_SendSMS($phone, $text, $sender, $needSave=1, $data='') {
   return $tmp;
 }
 
-    // возвращает статус сообщения с $messageId
+  // возвращает статус сообщения с $messageId
 function IQSMS_GetSMSStatus($messageId='') {
-  if (!$messageId) return 'no messageId';
+  if (!defined('IQSMS_URL') || !defined('IQSMS_API_LOGIN') || !defined('IQSMS_API_PASSWORD') || !$messageId) return false;
   $url = IQSMS_URL.'status?&login='.IQSMS_API_LOGIN.'&password='.IQSMS_API_PASSWORD.'&id='.$messageId;
   $curl = curl_init($url);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -47,8 +48,9 @@ function IQSMS_GetSMSStatus($messageId='') {
   return $tmp;
 }
 
-    // возвращает список доступных подписей в СМС
+  // возвращает список доступных подписей в СМС
 function IQSMS_GetSenders() {
+  if (!defined('IQSMS_URL') || !defined('IQSMS_API_LOGIN') || !defined('IQSMS_API_PASSWORD')) return false;
   $url = IQSMS_URL.'senders?&login='.IQSMS_API_LOGIN.'&password='.IQSMS_API_PASSWORD;
   $curl = curl_init($url);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -56,5 +58,3 @@ function IQSMS_GetSenders() {
   curl_close($curl);
   return $tmp;
 }
-
-?>
